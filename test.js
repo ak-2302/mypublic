7910428acb92838d5b7dfbb578aa5f4724b57334
ak-2302 <s2211879@s.tsukuba.ac.jp>
2024-02-25 17:36:51 +0900

    pre

--- test.js
+++ test.js
@@ -13,3 +13,47 @@ type.onchange = function () {
         private_setting.style.display = "none";
     }
 }
+const message_send = document.getElementById("message_send")
+message_send.addEventListener("click", () => {
+    console.log("clicked")
+    const input_area = document.getElementById("input_area")
+    input_area.value = ""
+    add_newMessage()
+})
+function add_newMessage() {
+    var newContent = document.createElement("pre")
+    newContent.className = "content"
+    newContent.innerHTML = "content"
+
+    var newMessage_text = document.createElement("pre")
+    newMessage_text.className = "message_text"
+    newMessage_text.appendChild(newContent)
+
+    var newText = document.createElement("div")
+    newText.className = "text"
+
+    var newName = document.createElement("p")
+    newName.className = "name"
+    newName.innerText = "User"
+
+    var newTime = document.createElement("p")
+    newTime.className = "time"
+    newTime.innerText = "today"
+
+    newText.appendChild(newName)
+    newText.appendChild(newTime)
+    newText.appendChild(newContent)
+
+    var newMessage = document.createElement("div")
+    newMessage.className = "message"
+
+    var newIcon = document.createElement("img")
+    newIcon.src = "https://cdn.discordapp.com/attachments/1209128947896287273/1209129995490828350/HsSEhISEhISEhISEhISEhISEhISEhISEhISEhISEhISEhISEhISEhKaxX9TPXFcYWdeKQAAAABJRU5ErkJggg.png?ex=65e5cd0f&is=65d3580f&hm=dee853664883004c0d7355b6b5d0ae50ec1dc40f466141f363e44a23884b752a&"
+    newIcon.className = "icon"
+
+    newMessage.appendChild(newIcon)
+    newMessage.appendChild(newText)
+
+    var parent = document.getElementById("message_area")
+    parent.insertBefore(newMessage, null)
+}
